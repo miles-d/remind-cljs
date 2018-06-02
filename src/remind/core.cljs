611b95f322961a370444f0678d07c74b6c1c7f99
Milosz Danczak <milosz.danczak@gmail.com>
2018-06-02 10:03:45 +0200

    init

--- src/remind/core.cljs
+++ src/remind/core.cljs
@@ -7,18 +7,42 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state (atom {:text "Hello world!"
+                          :topics #{{:title "hello"}
+                                    {:title "good-bye"}}}))
+
+;; return new state
+(defn add-topic [app-state title]
+  (let [old-topics (:topics app-state)
+        new-topic {:title title}
+        new-topics (conj old-topics new-topic)
+        new-state (assoc app-state :topics new-topics)]
+    new-state))
+
+(defn add-topic! [title]
+  (swap! app-state #(add-topic % title)))
 
 
-(defn hello-world []
+(defn list-item [title]
+  [:li title
+   [:button "Click me!"]])
+
+(defn remind-app []
   [:div
    [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h3 "Remind"]
+   [:ol
+    (for [topic (:topics @app-state)]
+      ^{:key topic} [list-item (:title topic)]
+      )]])
 
-(reagent/render-component [hello-world]
+(reagent/render-component [remind-app]
                           (. js/document (getElementById "app")))
 
+(def log (.-log js/console))
+
 (defn on-js-reload []
+  (log @app-state)
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
